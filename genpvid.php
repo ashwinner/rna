307c2f55efd1fe40dd24e546e82ef448fcf7e658
ashwin <ashwinner92@gmail.com>
2014-04-07 15:50:09 +0530

    Used phpseclib to read key created using openssl. Used jsencrypt on the client side to extract n and e from pem encoded public key.

--- genpvid.php
+++ genpvid.php
@@ -3,60 +3,41 @@
 <html>
 
 <body>
-
+<script src='js/bin/jsencrypt.min.js'></script>
 <h2>Generate your PVID</h2>
 
 <?php 
+    include('Crypt/RSA.php');
 
-if(isset($_POST['email']) && isset($_POST['pass']) && isset($_POST['pseudoID']))
-{
-	//check for valid email and password
-	//if valid, sign
-	//else return error
+    $rsa = new Crypt_RSA();
 
-	var_dump($_POST['email']);
-	echo "<br/>";
-	var_dump($_POST['pass']);
-	echo "<br/>";
-	var_dump($_POST['pseudoID']);
-	echo "<br/>";
-	echo "<script>
-		alert(localStorage.getItem('lastname'));
-		</script>
-		";
+    $rsa->loadKey(file_get_contents('key.pem'));
+    $rsa->setPublicKey();
 
-
-}
-else
-{
+    $publicKey = $rsa->getPublicKey();
 	echo "<form name='authenticate' action='genpvid.php' method='post' onsubmit='return blind()'>
 	Email : <input type='text' name='email'>
 	<br/>
 	PIN : &nbsp;&nbsp;&nbsp;<input type='password' name='pass'>
 	<br/>
-	Blinding Factor : <input type='text' name='bf'>
-	<button name=genbf onclick='genbf()'>Generate new BF</button>
-	<br/>
-	<input type='hidden' name='pseudoID' value=''>
+    <input type='hidden' name='pseudoID' value=''>
+    <input type='hidden' name='publicKey' value='$publicKey'>
 	<input type='submit' value='submit'>
 	</form>
 	";
-}
+
 ?>
 
 </body>
 </html>
 
 <script>
-function genbf() {
-document.getElementsByName('genbf')[0].value=100;
-return false;
-}
-function blind() {
-//var bf = bf();
-//var rn = rn();
-//document.getElementsByName('pseudoID')[0].value=blind(1000+rn, bf);
-localStorage.setItem("lastname", "Smith");
+    function blind() {
+        var crypt = new JSEncrypt();
+        crypt.setKey(document.getElementsByName('publicKey')[0].value);
+        var key = crypt.getKey();
+        var modulus = key.n;
+        alert(modulus);
 return true;
 }
 </script>
