367342e2090e2ece3e4bf023b18224bdae4ffbe9
nikitamathew <nikita.mathew@ymail.com>
2014-04-14 11:26:11 +0530

    parameters to send pin2 via email

---
+++ sendEmail.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+
+<?php
+include("Mail.php");
+
+/* SMTP server name, port, user/passwd */
+$smtpinfo["host"] = "smtp.gmail.com";
+$smtpinfo["port"] = "25";
+$smtpinfo["auth"] = true;
+$smtpinfo["username"] = "niksmd92@gmail.com";
+$smtpinfo["password"] = "nikita123rna3.1";
+/* Create the mail object using the Mail::factory method */
+//$mail_object =& Mail::factory("smtp", $smtpinfo);
+/* Ok send mail */
+//$mail_object->send($recipients, $headers, $mailmsg);
+?>
+
+
+
+
+
+
+
+
+
+
+
