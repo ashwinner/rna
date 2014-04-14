3b3a7ec4871467d350071ae65533313cb6e42294
nikitamathew <nikita.mathew@ymail.com>
2014-04-14 10:28:56 +0530

    form to generate pin2 for valid voters

---
+++ genpin2.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<body>
+
+<h2>Generate pin2</h2>
+
+<?php 
+
+echo "<form name='generatePin2' action='collector.php' method='post' onsubmit='return generate_pin2()'>
+	Enter your Email ID : <input type='text' name='email'>
+	<br/>
+	<input type='submit' value='submit'>
+	</form>
+	";
+
+?>
+
+</body>
+</html>
