3ca3422681d72411b39acf2408a93c6bd5dbcc33
reshmap <itzreshma573@gmail.com>
2014-04-16 11:37:33 +0530

    Prefixed PVID and encrypted Vote

--- ballot.php
+++ ballot.php
@@ -17,6 +17,7 @@
     
     $n = new Math_BigInteger($collectorKey['rsa']['n'], 256);
     $e = new Math_BigInteger($collectorKey['rsa']['e'], 256);
+	$d = new Math_BigInteger($collectorKey['rsa']['d'], 256);
 
 	echo "<form name='authenticate' action='collector.php' method='post' onsubmit='return EncryptAndBlind()'>
 	Email : <input type='text' name='email'>
@@ -30,6 +31,7 @@
     	<br/>
        	<input type='hidden' name='n' value='$n'>
     	<input type='hidden' name='e' value='$e'>
+	<input type='hidden' name='d' value='$d'>
 	<input type='submit' value='submit'>
 	</form>
 	";
@@ -45,7 +47,10 @@
         var n = new BigInteger(document.getElementsByName('n')[0].value);
         var e = new BigInteger(document.getElementsByName('e')[0].value);
 	var PVID=new BigInteger(localStorage.getItem('PVID'));
-	
+	var PVIDprefixed= new BigInteger("1110" + PVID.toString());
+	//var PVIDprefixed=PVID;
+	var key= localStorage.getItem('key');
+	console.log("key :" +key);	
         var rng = new SecureRandom();
 
         var blindingFactor;
@@ -58,12 +63,12 @@
         
         var vote = new BigInteger(document.getElementsByName('blindedEncryptedVote')[0].value);
 	var encryptedVote= vote;
-	localStorage.setItem('encryptedVote', encryptedVote.toString());
-	 
+//	localStorage.setItem('encryptedVote', encryptedVote.toString());
+	var encryptedVotePrefixed= new BigInteger("1110" + encryptedVote.toString());	 
         //console.log("Vote : " + vote);
 
-        var blindedEncryptedVote = blindingFactor.modPow(e, n).multiply(encryptedVote).mod(n);
-	var blindedPVID= blindingFactor.modPow(e, n).multiply(PVID).mod(n);
+        var blindedEncryptedVote = blindingFactor.modPow(e, n).multiply(encryptedVotePrefixed).mod(n);
+	var blindedPVID= blindingFactor.modPow(e, n).multiply(PVIDprefixed).mod(n);
         //console.log("blindedNo : " + blindedEncryptedVote);
         document.getElementsByName('blindedEncryptedVote')[0].value=blindedEncryptedVote;
 	//console.log("blindedPVID" + blindedPVID);
